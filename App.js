c3daf29efcccfe25119e1748b41683e772ff9eb3
dulcegomez01 <169126204+dulcegomez01@users.noreply.github.com>
2025-02-26 00:10:59 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// App.tsx o App.js
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
